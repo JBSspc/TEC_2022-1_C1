9c55c899b0c0ae7227f46484611cbd0957a2a64a
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-09-07 19:14:10 -0500

    Add files via upload

---
+++ tarea03SPC.cpp
@@ -0,0 +1,48 @@
+/*==============================================================================================
+Autor: Sofía Palacios Cuevas
+Email: sofiapalacioscuevas@comunidad.unam.mx
+01 SEP 2021
+==============================================================================================*/
+
+//Realizar la multiplicación de todos los elementos de un arreglo de entrada con n elementos.
+
+procedure multiply(n):
+// Input: n: número de elementos del arreglo
+// Output: multiplicacion: multiplicación de todos los elementos del arreglo
+
+multiplicacion = 1 // para que no se afecte la multiuplicaión
+A[n] // definimos el arreglo
+
+for(i = 0; i < n; i++): // empezamos a numerar desde 0 hasta n-1; será iteraritvo
+    multiplicacion *= A[n]:
+return nultiplicacion
+
+//Complejidad
+
+//Calcular el promedio de un arreglo con n números enteros de entrada.
+
+procedure promedio(n):
+// Input: n: número de elementos del arreglo
+// Output: promedio: promedio de todos los elementos del arreglo
+
+suma = 0 // la suma que incia el proceso del promedio es incialmente = 0
+
+B[n]// definimos el arreglo
+for(i = 0, i<n;i++): // seguimos la conveción para numerar de 0 a n-1; será iterativo
+    suma += B[n]:
+    promedio = B[n]/n:
+return promedio
+
+// Complejidad
+
+// Obtener el elemento más pequeño en un arreglo de n elementos.
+//Complejidad
+
+//Obtener el elemento más grande en un arreglo de n elementos
+//Complejidad
+
+//Determinar si un número es primo o no lo es
+//Complejidad
+
+
+
